be6e80071ac8b0de8ee962bb7362b3ec20b55ba3
Peter Wilton <peterrwilton@gmail.com>
2015-11-14 19:58:36 -0500

    added triploid asexual simulation feature

--- add_asex.py
+++ add_asex.py
@@ -9,6 +9,7 @@ parser = argparse.ArgumentParser()
 
 parser.add_argument('filename', type = str, help = 'filename containing ms-format loci (- for STDIN)')
 parser.add_argument('divtime', type = float, help = 'number of generations since transition from sexual reproduction, in units of 2*N0')
+parser.add_argument('--triploid', action='store_true')
 
 args = parser.parse_args()
 
@@ -23,43 +24,103 @@ firstline = fin.next().strip()
 spfirstline = firstline.split(' ')
 numSites = int(spfirstline[spfirstline.index('-r')+2])
 theta = float(spfirstline[spfirstline.index('-t')+1])
-if spfirstline[1] != '2':
+if not args.triploid and spfirstline[1] != '2':
     raise ValueError('expected 2 haplotypes in ms output format')
+if args.triploid and spfirstline[1] != '3':
+    raise ValueError('expected 3 haplotypes in ms output format for triploids')
 
 print firstline
-for line in fin:
-    line = line.strip()
-    if re.match('//', line):
-        line = fin.next().strip()  # segsites: 12398
-        segNumMuts = int(line.split(' ')[1])
-        line = fin.next().strip()  # positions: 0.0005 0.0011 
-        positions = np.array([float(el) for el in line.split(' ')[1:]], dtype = np.float64)
-        line = fin.next().strip() # 01011110010100010110101001000001001...
-        firstHap = [int(el) for el in list(line)]
-        line = fin.next().strip()
-        # secondHap = line
+if not args.triploid:
+    for line in fin:
+        line = line.strip()
+        if re.match('//', line):
+            line = fin.next().strip()  # segsites: 12398
+            segNumMuts = int(line.split(' ')[1])
+            line = fin.next().strip()  # positions: 0.0005 0.0011 
+            positions = np.array([float(el) for el in line.split(' ')[1:]], dtype = np.float64)
+            line = fin.next().strip() # 01011110010100010110101001000001001...
+            firstHap = np.array([int(el) for el in list(line)])
+            line = fin.next().strip()
+            # secondHap = line
 
-        # simulate asexual / Meselson-effect mutations
-        numAsexMuts = npr.poisson(args.divtime * theta)
-        asexMutPositions = npr.rand(numAsexMuts)  # npr.rand(n) simulates n Unif(0,1)'s
-        asexTypes = npr.choice(np.array([0,1]), size=numAsexMuts, replace = True)
+            # simulate asexual / Meselson-effect mutations
+            numAsexMuts = npr.poisson(args.divtime * theta)
+            asexMutPositions = npr.rand(numAsexMuts)  # npr.rand(n) simulates n Unif(0,1)'s
+            asexTypes = npr.choice(np.array([0,1]), size=numAsexMuts, replace = True)
 
-        # adding the asexual mutations
-        positions = np.concatenate((positions, asexMutPositions))
-        firstHap = np.concatenate((firstHap, asexTypes))
-        sortedIdxs = positions.argsort()
-        positions = positions[sortedIdxs]
-        firstHap = firstHap[sortedIdxs]
-        secondHap = np.bitwise_xor(firstHap, 1)
-        newNumSegSites = positions.size
+            # adding the asexual mutations
+            positions = np.concatenate((positions, asexMutPositions))
+            firstHap = np.concatenate((firstHap, asexTypes))
+            sortedIdxs = positions.argsort()
+            positions = positions[sortedIdxs]
+            firstHap = firstHap[sortedIdxs]
+            secondHap = np.bitwise_xor(firstHap, 1)
+            newNumSegSites = positions.size
 
-        # print the new locus
-        print '//'
-        print 'segsites: {}'.format(newNumSegSites)
-        print 'positions: {}'.format(' '.join([str(el) for el in positions]))
-        print ''.join([str(el) for el in list(firstHap)])
-        print ''.join([str(el) for el in list(secondHap)])
+            # print the new locus
+            print '//'
+            print 'segsites: {}'.format(newNumSegSites)
+            print 'positions: {}'.format(' '.join([str(el) for el in positions]))
+            print ''.join([str(el) for el in list(firstHap)])
+            print ''.join([str(el) for el in list(secondHap)])
 
-    else:
-        print line
+        else:
+            print line
 
+elif args.triploid:
+    for line in fin:
+        line = line.strip()
+        if re.match('//', line):
+            line = fin.next().strip()  # segsites: 12398
+            segNumMuts = int(line.split(' ')[1])
+            line = fin.next().strip()  # positions: 0.0005 0.0011 
+            positions = np.array([float(el) for el in line.split(' ')[1:]], dtype = np.float64)
+            line = fin.next().strip() # 01011110010100010110101001000001001...
+            firstHap = np.array([int(el) for el in list(line)])
+            line = fin.next().strip()
+            secondHap = np.array([int(el) for el in list(line)])
+            line = fin.next().strip()
+            thirdHap = np.array([int(el) for el in list(line)])
+
+            # simulate asexual / Meselson-effect mutations
+            numAsexMuts = npr.poisson(args.divtime * 3.0/2.0*theta)  # note 3/2 multiplier
+            asexMutPositions = npr.rand(numAsexMuts)  # npr.rand(n) simulates n Unif(0,1)'s
+            asexTypes = npr.choice(np.array([0,1,2]), size=numAsexMuts, replace = True)
+            asexFirstHapTypes = np.zeros(asexTypes.shape[0], dtype = np.int)
+            asexSecondHapTypes = np.zeros(asexTypes.shape[0], dtype = np.int)
+            asexThirdHapTypes = np.zeros(asexTypes.shape[0], dtype = np.int)
+            asexFirstHapTypes[asexTypes == 0] = 1
+            asexSecondHapTypes[asexTypes == 1] = 1
+            asexThirdHapTypes[asexTypes == 2] = 1
+
+
+            # adding the asexual mutations
+            positions = np.concatenate((positions, asexMutPositions))
+            firstHap = np.concatenate((firstHap, asexFirstHapTypes))
+            secondHap = np.concatenate((secondHap, asexSecondHapTypes))
+            thirdHap = np.concatenate((thirdHap, asexThirdHapTypes))
+
+            sortedIdxs = positions.argsort()
+
+            positions = positions[sortedIdxs]
+            firstHap = firstHap[sortedIdxs]
+            secondHap = secondHap[sortedIdxs]
+            thirdHap = thirdHap[sortedIdxs]
+            newNumSegSites = positions.size
+
+            # print the new locus
+            print '//'
+            print 'segsites: {}'.format(newNumSegSites)
+            print 'positions: {}'.format(' '.join([str(el) for el in positions]))
+            print ''.join([str(el) for el in list(firstHap)])
+            print ''.join([str(el) for el in list(secondHap)])
+            print ''.join([str(el) for el in list(thirdHap)])
+
+            hapMat = np.vstack((firstHap,secondHap,thirdHap))
+            gencounts = np.bincount(hapMat.sum(axis=0))
+            #R = float(gencounts[1])/(gencounts.sum())
+            #print (3.0*R-2)/(3*(1-R))
+            #sys.exit(0)
+
+        else:
+            print line
