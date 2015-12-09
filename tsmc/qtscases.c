128bd41f587bae5da59d21fb8a3a02704860ea79
Peter Wilton <peterrwilton@gmail.com>
2015-12-09 14:55:21 -0500

    fixed Es3 -> Es2 bug in case G

--- tsmc/qtscases.c
+++ tsmc/qtscases.c
@@ -252,7 +252,7 @@ double qts_case_G(Hmm * hmm, int i, int j, int k, int l)
             (1.0 - exp(-3*io[a])) * lam[a]/3.0;
     }
 
-    prob = 1.0/(2.0*es3[rowIdx]+es3[rowIdx]) * ( 1.0/lam[k] *
+    prob = 1.0/(2.0*es2[rowIdx]+es3[rowIdx]) * ( 1.0/lam[k] *
             (sum1 + (1-exp(-3*(es3[rowIdx]-t[i])/lam[i])) * lam[i]/3.0) * 
             lam[k]/2.0 * (1-exp(-2*(t[k+1]-es3[rowIdx])/lam[k])) +
             t[k+1] - es3[rowIdx] - 
