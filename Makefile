0b56319e4f9e5c4381175de596fbb5928048ce51
Peter Wilton <peterrwilton@gmail.com>
2015-12-11 20:01:12 -0500

    added Data entry functions, objects and methods
    need to add free() functionality still

--- Makefile
+++ Makefile
@@ -1,11 +1,7 @@
 all: tsmc
 
 tsmc:
-	gcc tsmc.c hmm.c definitions.c qtscases.c -lm -o tsmc
+	gcc tsmc.c hmm.c definitions.c qtscases.c data.c -lm -o tsmc
 
 debug:
-	gcc tsmc.c hmm.c definitions.c qtscases.c -lm -o tsmc -g
-
-simt3t2: simt3t2.c simt3t2.h random.c random.h
-	gcc simt3t2.c random.c -O2 -lm -o simt3t2
-
+	gcc tsmc.c hmm.c definitions.c qtscases.c data.c -lm -o tsmc -g
--- data.h
+++ data.h
@@ -18,7 +18,8 @@
  * -----------------------
  *   - read data
  *   - allocate space for storing chroms
- *   - allocate a new Seq
+ *   - add a Seq
+ *   - init, allocate a new Seq
  * 
  * Seq struct needs:
  * -----------------
@@ -27,7 +28,19 @@
  * - index (int)
  * - data (char *)
  *
+ * Seq methods/functions:
+ * ----------------------
+ * - read seq from FILE *
+ * - increment Seq length
+ * - (re)allocate space for seq
  */
+
+// global increment size
+typedef enum
+{
+    polarized = 0,
+    nonpolarized
+} SeqType;
 
 typedef struct
 {
@@ -37,12 +50,44 @@ typedef struct
     char * data;
 } Seq;
 
+
 typedef struct
 {
     int numSeqs;
     int maxNumSeqs;
-    int polarized;
-    Seq * chroms;
+    SeqType seqtype;
+    Seq * seqs;
 } Data;
+
+/*
+ * Data struct needs:
+ * -------------------
+ * - number of seqs (int)
+ * - type of data (polarized or not) (int or typedef'ed enum)
+ * - data (Seq *)
+ * - current number of allocated slots (int)
+ *
+ * Data methods/functions:
+ * -----------------------
+ *   - init
+ *   - read data
+ *   - allocate space for storing chroms
+ *   - add a Seq
+ *   - init, allocate a new Seq
+ */
+
+inline void Seq_add_buffer_size(Seq * seq, int increment);
+inline void Seq_increment_length(Seq * seq);
+void Seq_init(Seq * seq, int initSize, int index);
+void Seq_read_seq(Seq * seq, FILE * fin, SeqType type);
+void Seq_print_seq(Seq * seq, int idx);
+void Seq_free(Seq * seq);
+void Data_init(Data * dat, SeqType seqtype);
+void Data_add_seq_buffer(Data * dat, int increment);
+void Data_add_seq_buffer(Data * dat, int increment);
+Seq * Data_get_seq(Data * dat);
+void Data_read_seq(Data * dat, FILE * fin);
+void Data_read_data(Data * dat, FILE * fin);
+void Data_print_seqs(Data * dat);
 
 #endif
