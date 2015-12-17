fbf1e71ae8fa28231954d0f41916790c9339aecf
Peter Wilton <peterrwilton@gmail.com>
2015-12-17 14:16:09 -0600

    fixed bug in likelihood calculation
    added check prototypes to header

--- tsmc/em.h
+++ tsmc/em.h
@@ -27,5 +27,10 @@ void Em_get_expectations(Em * em);
 void Em_iterate(Em * em);
 double Em_get_expected_log_likelihood(Em * em, const int hmmIdx);
 double objective_function(double * par);
+void Em_print_forward(Em * em);
+void Em_print_backward(Em * em);
+void Em_print_norm_const(Em * em);
+void Em_print_gamma(Em * em);
+void Em_print_expect(Em * em);
 
 #endif
