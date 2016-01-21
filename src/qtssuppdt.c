c149f31a62580d4c80aedccdfeef503a4982db07
Peter Wilton <peterrwilton@gmail.com>
2016-01-21 12:00:53 -0500

    added conditional expressions for io[n]
    io[n] should be INFINITY, but not allocated and not stored as such, so
    using somehting like this:
    
    ((l == n) ? 1.0 : (1.0 - exp(-3.0*io[l])))

--- src/qtssuppdt.c
+++ src/qtssuppdt.c
@@ -5,6 +5,7 @@
 #include "definitions.h"
 #include "hmm.h"
 #include "qtscases.h"
+#include "qtssuppdt.h"
 
 double qts_case_A_supp(Hmm * hmm, int i, int j, int k, int l)
 {
@@ -31,7 +32,7 @@ double qts_case_A_supp(Hmm * hmm, int i, int j, int k, int l)
         exp(-3*get_omega_interval_Es3(hmm, 0, i, j)) * 
         exp(-2*get_omega_Es3_Es2(hmm, i, j)) * 
         exp(-get_omega_Es2_interval(hmm, l, i, j)) *
-        (1.0 - exp(-io[l]));
+        ((l == n) ? 1.0 : (1.0 - exp(-io[l])));
 
     assert(0 <= prob && prob <= 1);
     return prob;
@@ -62,7 +63,7 @@ double qts_case_B_supp(Hmm * hmm, int i, int j, int k, int l)
     prob = 1.0/(2.0*Es2+Es3-D3) * lamd * (exp(-D3/lamd)-1.0) * 
         exp(-3*get_omega_interval_Es3(hmm, 0, i, j)) * 
         exp(-2*get_omega_Es3_interval(hmm, l, i, j)) * 
-        1.0/2.0 * (1.0 - exp(-2.0*io[l]));
+        1.0/2.0 * ((l == n) ? 1.0 : (1.0 - exp(-2.0*io[l])));
 
     assert(0 <= prob && prob <= 1);
     return prob;
@@ -94,7 +95,7 @@ double qts_case_C_supp(Hmm * hmm, int i, int j, int k, int l)
 
     prob = (lamd * (exp(-D3/lamd)-1.0)) / (2.0*Es2+Es3-D3) * 2.0/3.0 *
         exp(-3.0*get_omega_interval_interval(hmm, 0, k)) *
-        (1.0 - exp(-3.0*io[k]));
+        ((k == n) ? 1.0 : (1.0 - exp(-3.0*io[k])));
 
     assert(0 <= prob && prob <= 1);
     return prob;
@@ -125,7 +126,7 @@ double qts_case_D_supp(Hmm * hmm, int i, int j, int k, int l)
 
     prob = (lamd * (exp(-D3/lamd)-1.0)) / (2.0*Es2+Es3-D3) * 2.0/3.0 *
         exp(-3.0*get_omega_interval_interval(hmm, 0, k)) *
-        (1.0 - exp(-3.0*io[k]));
+        ((k == n) ? 1.0 : (1.0 - exp(-3.0*io[k])));
 
     assert(0 <= prob && prob <= 1);
     return prob;
@@ -157,7 +158,7 @@ double qts_case_E_supp(Hmm * hmm, int i, int j, int k, int l)
     prob = (lamd * (exp(-D3/lamd)-1.0)) / (2.0*Es2+Es3-D3) *
         exp(-3.0*get_omega_interval_Es3(hmm, 0, i, j)) * 
         exp(-2.0*get_omega_Es3_interval(hmm, k, i, j)) *
-        (1.0 - exp(-2.0*io[k]));
+        ((k == n) ? 1.0 : (1.0 - exp(-2.0*io[k])));
 
     assert(0 <= prob && prob <= 1);
     return prob;
@@ -189,7 +190,7 @@ double qts_case_F_supp(Hmm * hmm, int i, int j, int k, int l)
         exp(-3.0*get_omega_interval_Es3(hmm, 0, i, j)) *
         exp(-2.0*get_omega_Es3_Es2(hmm, i, j)) *
         exp(-get_omega_Es2_interval(hmm, l, i, j)) *
-        (1.0 - exp(-io[l]));
+        ((l == n) ? 1.0 :(1.0 - exp(-io[l])));
 
     assert(0 <= prob && prob <= 1);
     return prob;
@@ -339,11 +340,16 @@ double qts_case_I_supp(Hmm * hmm, int i, int j, int k, int l)
 
     double prob;
 
+    assert(l <= n);
+    DEBUGREPORTI(l);
+    DEBUGREPORTI(n);
+    DEBUGREPORTI(l == n);
+
     prob = (lamd * (exp(-D3/lamd)-1.0)) / (2.0*Es2+Es3-D3) * 
         exp(-3.0*get_omega_interval_Es3(hmm, 0, i, j)) *
         exp(-2.0*get_omega_Es3_Es2(hmm, i, j)) *
         exp(-get_omega_Es2_interval(hmm, l, i, j)) *
-        (1.0 - exp(-io[l]));
+        ((l == n) ? 1.0 : (1.0 - exp(-io[l])));
 
     assert(0 <= prob && prob <= 1);
     return prob;
@@ -375,7 +381,7 @@ double qts_case_I2_supp(Hmm * hmm, int i, int j, int k, int l)
         exp(-3.0*get_omega_interval_Es3(hmm, 0, i , j)) *
         exp(-2.0*get_omega_Es3_Es2(hmm, i, j)) *
         exp(-get_omega_Es2_interval(hmm, l, i, j)) *
-        (1.0 - exp(-io[l]));
+        ((l == n) ? 1.0 : (1.0 - exp(-io[l])));
 
     assert(0 <= prob && prob <= 1);
     return prob;
@@ -405,7 +411,7 @@ double qts_case_J_supp(Hmm * hmm, int i, int j, int k, int l)
 
     prob = (lamd * (exp(-D3/lamd)-1.0)) / (2.0*Es2+Es3-D3) * 2.0/3.0 *
         exp(-3.0*get_omega_interval_interval(hmm, 0, k)) *
-        (1.0 - exp(-3.0*io[k]));
+        ((k == n) ? 1.0 : (1.0 - exp(-3.0*io[k])));
 
     assert(0 <= prob && prob <= 1);
     return prob;
@@ -435,7 +441,7 @@ double qts_case_J2_supp(Hmm * hmm, int i, int j, int k, int l)
 
     prob = (lamd * (exp(-D3/lamd)-1.0)) / (2.0*Es2+Es3-D3) * 2.0/3.0 *
         exp(-3.0*get_omega_interval_interval(hmm, 0, k)) *
-        (1.0 - exp(-3.0*io[k]));
+        ((k == n) ? 1.0 : (1.0 - exp(-3.0*io[k])));
 
     assert(0 <= prob && prob <= 1);
     return prob;
