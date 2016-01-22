25fd0f25c0f0371252fe989c2e1a4a3e0542b747
Peter Wilton <peterrwilton@gmail.com>
2016-01-22 12:00:38 -0500

    adding qtssuppdt.h, forgotten earlier

---
+++ src/qtssuppdt.h
@@ -0,0 +1,21 @@
+#ifndef QTSSUPP_H
+#define QTSSUPP_H
+
+#include "hmm.h"
+
+double qts_case_A_supp(Hmm * hmm, int i, int j, int k, int l);
+double qts_case_B_supp(Hmm * hmm, int i, int j, int k, int l);
+double qts_case_C_supp(Hmm * hmm, int i, int j, int k, int l);
+double qts_case_D_supp(Hmm * hmm, int i, int j, int k, int l);
+double qts_case_E_supp(Hmm * hmm, int i, int j, int k, int l);
+double qts_case_F_supp(Hmm * hmm, int i, int j, int k, int l);
+double qts_case_G_supp(Hmm * hmm, int i, int j, int k, int l);
+double qts_case_G2_supp(Hmm * hmm, int i, int j, int k, int l);
+double qts_case_H_supp(Hmm * hmm, int i, int j, int k, int l);
+double qts_case_H2_supp(Hmm * hmm, int i, int j, int k, int l);
+double qts_case_I_supp(Hmm * hmm, int i, int j, int k, int l);
+double qts_case_I2_supp(Hmm * hmm, int i, int j, int k, int l);
+double qts_case_J_supp(Hmm * hmm, int i, int j, int k, int l);
+double qts_case_J2_supp(Hmm * hmm, int i, int j, int k, int l);
+
+#endif
