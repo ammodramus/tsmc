ca632ffd256251d8c7b5b210dab8c377ea9da2a3
Peter Wilton <peterrwilton@gmail.com>
2016-01-28 10:48:51 -0500

    changing %g to %f in diptripsurface test

--- src/diptriptestsurface.c
+++ src/diptriptestsurface.c
@@ -85,7 +85,7 @@ int main(int argc, char ** argv)
             par[4] = sqrt(em.hmm[em.hmmFlag].Td + D3);
             par[5] = sqrt(lamd);
             expect = objective_function_dt(par);
-            printf("%.15g\t%.15g\t%.15g\n", D3, lamd, expect);
+            printf("%.15f\t%.15f\t%.15f\n", D3, lamd, expect);
         }
     }
 
